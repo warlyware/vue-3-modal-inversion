a475f9b03c248ef260b47cf76943e63c45e2e949
Dan Ward <danward@gmail.com>
2021-03-02 09:32:01 -0800

    Extract modal form into own component

--- src/components/ReviewReplyModal.vue
+++ src/components/ReviewReplyModal.vue
@@ -3,18 +3,7 @@
     v-if="showModal"
     title="Review Reply"
   >
-    <form>
-      <textarea
-        id="story"
-        v-model="formData.replyText"
-        name="story"
-        rows="5"
-        cols="33"
-      />
-      <button @click="save">
-        Save
-      </button>
-    </form>
+    <review-reply-modal-form />
     <template #footer>
       FOOTER STUFF
     </template>
@@ -23,47 +12,33 @@
 
 <script>
 import {
-  nextTick,
   reactive,
   toRefs
 } from 'vue'
 
 import { REVIEW_REPLY } from '../constants/modals/names'
 
+import ReviewReplyModalForm from './ReviewReplyModalForm.vue'
+
 import useModal from '../utils/use-modal'
 import WwModal from './WwModal.vue'
 
 export default {
   components: {
+    ReviewReplyModalForm,
     WwModal
   },
   setup() {
-    const {
-      activeModal, closeModal, setupModal
-    } = useModal()
+    const { setupModal } = useModal()
 
     const state = reactive({
-      showModal: false,
-      formData: {
-        replyText: ''
-      }
+      showModal: false
     })
-
-    const clearForm = () => {
-      state.formData.replyText = ''
-    }
-
-    const save = async () => {
-      const { formData } = toRefs(state)
-      closeModal(formData.value)
-      await nextTick()
-      clearForm()
-    }
 
     state.showModal = setupModal(REVIEW_REPLY)
 
     return {
-      ...toRefs(state), activeModal, closeModal, save
+      ...toRefs(state)
     }
   }
 }
---
+++ src/components/ReviewReplyModalForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <form>
+    <textarea
+      id="story"
+      v-model="formData.replyText"
+      name="story"
+      rows="5"
+      cols="33"
+    />
+    <button @click="save">
+      Save
+    </button>
+  </form>
+</template>
+
+<script>
+import {
+  reactive,
+  toRefs,
+  nextTick
+} from 'vue'
+
+import useModal from '../utils/use-modal'
+
+export default {
+  setup() {
+    const { closeModal } = useModal()
+
+    const state = reactive({
+      showModal: false,
+      formData: {
+        replyText: ''
+      }
+    })
+
+    const clearForm = () => {
+      state.formData.replyText = ''
+    }
+
+    const save = async () => {
+      const { formData } = toRefs(state)
+      closeModal(formData.value)
+      await nextTick()
+      clearForm()
+    }
+
+    return { ...toRefs(state), save }
+  }
+}
+</script>
+
+<style>
+
+</style>
